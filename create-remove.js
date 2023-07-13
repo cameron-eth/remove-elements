f9f1aa45fe892ad27dd6cd5597ca5cab603d7379
Alex Alfadel <alexalfadel@gmail.com>
2023-07-12 17:01:11 -0700

    first prob

--- create-remove.js
+++ create-remove.js
@@ -1,4 +1,5 @@
 /****************************** ADD DOG BUTTON ******************************/
+
 const add = document.getElementById("add");
 add.addEventListener("click", async () => {
     try {
@@ -8,15 +9,42 @@ add.addEventListener("click", async () => {
         const url = data.message; // URL of new dog image
 
         /*--------------- Get breed (Hint: Parse from URL) ---------------- */
-        // Your code here
+        // console.log(url);
+        const breedArray = url.split('/');
+        const breed = breedArray[breedArray.length - 2];
+        // console.log(breed);
 
         /*------------ Create new dog card with the url above ------------- */
         /* (use the HTML structure for the current dog image in the index.html
             file to create a new image with the url) */
         // Your code here
 
+        const currImg = document.querySelector("img");
+        currImg.src = url;
+        // console.log(currImg);
+
+        const breedCaption = document.querySelector("figcaption");
+        breedCaption.innerText = breed;
+
         /* Add the new dog card as a child to the ul in the .gallery element */
         // Your code here
+        const newLi = document.createElement("li");
+        const figure = document.createElement("figure")
+        const image = document.createElement("img")
+        const figcaption = document.createElement("figcaption");
+
+        image.src = url;
+        figcaption.innerText = breed;
+
+        figure.appendChild(image);
+        figure.appendChild(figcaption)
+
+        newLi.appendChild(figure);
+
+        const ul = document.querySelector("ul");
+        // newLi.innerHTML = `<figure>${currImg}<figcaption>${breed}</figcaption></figure>`;
+        ul.appendChild(newLi);
+
 
     } catch (e) {
         console.log("Couldn't fetch dog :(")
@@ -42,3 +70,5 @@ removeLast.addEventListener("click", () => {
     /*-------------------- Remove the last dog card ----------------------- */
     // Your code here
 });
+
+// window.onload();
